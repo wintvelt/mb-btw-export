0da34d1be69cfd7cb41d2b97cacd72ed88187bf2
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-03-18 09:00:52 +0100

    tryout cors

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,8 +1,21 @@
+const baseHeaders =
+{
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE',
+    'Access-Control-Max-Age': '86400',
+    'Access-Control-Allow-Credentials': true,
+    'Access-Control-Allow-Headers':
+        'X-Requested-With, X-HTTP-Method-Override, Content-Type, Authorization, Origin, Accept'
+
+};
+
 module.exports.response = (statusCode, bodyOrString) => {
     const body = typeof bodyOrString === 'string' ?
         bodyOrString
         : JSON.stringify(bodyOrString, null, 2);
     return {
+        headers: baseHeaders,
         statusCode,
         body
     }
